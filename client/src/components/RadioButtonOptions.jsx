167701eb8a1d74fa52dd1b4c3390fd3ef50ff315
CaptainMorgan1992 <kimberly.morgan1992@outlook.com>
2023-04-03 20:46:36 +0200

    Added stuff to rent-a-friend-page

--- client/src/components/RadioButtonOptions.jsx
+++ client/src/components/RadioButtonOptions.jsx
@@ -1,16 +1,35 @@
-import {React, useState, useEffect, useContext} from "react";
+import React, {useContext} from "react";
 import GlobalContext from "../../GlobalContext.jsx";
 
-export default function ({details}) {
-    let {activities, setActivities} = useContext(GlobalContext)
-    //const {title, duration} = details
-console.log(
-    activities.map(activity => <input title={activity.title} type={"radio"}></input>))
+export default function () {
+    const {activities, setActivities} = useContext(GlobalContext)
 
-    return <div>
-        {
-            activities.map(activity => <p title={activity.title}>{activity.title}</p>)
-        }
+    return <div id="standing-area">
+        <label>Choose an activity </label>
+        <select value={activities} onChange={(e) => {
+            const selectedValue = e.target.value;
+            setActivities(selectedValue)
+        }}>
+            {
+                activities.map(activity => <option value={activity.title}>{activity.title}</option>)
+            }
+        </select>
+        <div id="button-div">
+            <button className={"booking-confirmation-button"}> Hire friend</button>
+        </div>
+
     </div>
 
 }
+
+
+/*
+<option value="1">{activities[0].title}</option>
+            <option value="2">{activities[1].title}</option>
+            <option value="3">{activities[2].title}</option>
+
+
+ {
+            activities.map(activity => <p title={activity.title}>{activity.title}</p>)
+        }
+ */
