2b0c60ec51a9ea0b393c8aceaa6bf87efd26f465
Erimara <erimarandersson@gmail.com>
2023-04-10 15:45:17 +0200

    added complete functionality to order lifecycle

--- client/src/components/Orders.jsx
+++ client/src/components/Orders.jsx
@@ -1,15 +1,11 @@
 import {useContext, useEffect} from "react";
 import GlobalContext from "../GlobalContext.jsx";
-import AdminOrderCard from "./PlacedOrderCard.jsx";
-import FriendCardDetailed from "./FriendCardDetailed.jsx";
 import PlacedOrderCard from "./PlacedOrderCard.jsx";
 
 
 
 export default function () {
     const {orders,auth,getCurrentUser,currentUser} = useContext(GlobalContext)
-    console.log(orders)
-    console.log(currentUser)
     useEffect( () => {
         getCurrentUser()
     }, [])
@@ -21,30 +17,33 @@ export default function () {
 
 function getUnconfirmedOrders(){
         const filteredOrders = orders.filter(order => {
-            if (order.confirmed === false && auth.loggedIn && auth.email === "johnnyjohnson@example.com") {
+            if (order.confirmed === false &&
+                auth.loggedIn &&
+                currentUser.admin) {
                 return true;
             }
         });
-
         return filteredOrders.map(order => <PlacedOrderCard confirmOrder={order}/>);
 
     }
 
     function getUsersConfirmedOrders() {
-        const filteredOrders = orders.filter(order => {
-            console.log(order.user._id)
-            console.log(currentUser._id)
-            if (order.confirmed === true && order.user._id === currentUser._id) {
-                return true;
-            }
-            return false;
-        });
-
-        return filteredOrders.map(order => <PlacedOrderCard confirmOrder={order}/>);
-    }
-
+            const filteredOrders = orders.filter((order) => {
+                return (
+                    order.confirmed === true &&
+                    auth.loggedIn &&
+                    !currentUser.admin &&
+                    order.user.some((user) => user._id === currentUser._id)
+                );
+            });
+            return filteredOrders.map((order) => (
+                <PlacedOrderCard confirmOrder={order} />
+            ));
+        }
 
 }
+
+
 
 
 
--- client/src/components/PlacedOrderCard.jsx
+++ client/src/components/PlacedOrderCard.jsx
@@ -42,7 +42,6 @@ export default function ({confirmOrder}){
 
     function sendConfirmation(){
         console.log("Send confirmation")
-        //confirmOrder.confirmed = true
         setAdminConfirm(true)
         adminConfirmOrder(_id,friend,user,activity,time,true)
         console.log(_id,friend,user,activity,time,true)
